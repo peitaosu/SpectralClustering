a7fe5caf0bd4334a6e8e945d0cdb1d6aa34e66f2
Tony Su <peitaosu@163.com>
2020-01-19 22:00:34 +0800

    matplotlib to visualize

--- visualize/visualize.py
+++ visualize/visualize.py
@@ -1,4 +1,5 @@
 import os, sys
+import matplotlib.pyplot as plt
 
 class Visualizer():
     
@@ -9,11 +10,20 @@ class Visualizer():
     def get_data_from_file(self, input):
         self.input = input
         with open(input) as in_file:
-            self.data = []
+            self.data = {
+                "X": [],
+                "Y": []
+            }
             for line in in_file.readlines():
-                self.data.append((line.split("\t")[0], line.split("\t")[1]))
-       
+                self.data["X"].append(float(line.split("\t")[0]))
+                self.data["Y"].append(float(line.split("\t")[1]))
+    
+    def visualize(self):
+        plt.plot(self.data["X"], self.data["Y"], 'ro')
+        plt.axis([450, 600, -1000, 5000])
+        plt.show()
+
 if __name__ == "__main__":
     input = sys.argv[1]
     visualizer = Visualizer(input)
-    print(visualizer.data)
+    visualizer.visualize()
