b0cf5edbece6d3bfa0fd57538e9a7232484aed4e
Tony Su <peitaosu@163.com>
2020-02-10 22:42:08 +0800

    move preprocess to separate file

--- visualize/preprocess.py
+++ visualize/preprocess.py
@@ -1,7 +1,19 @@
+import os, path
+
 class Preprocesser():
     def __init__(self):
-        pass
+        self.data = {
+            "X": [],
+            "Y": []
+        }
 
-    def process(self, in_dict):
-        out_dict = in_dict
-        return out_dict
+    def process(self, input):
+        if not os.path.isfile(input):
+            print(input + " is not exists.")
+            sys.exit(-1)
+        with open(input) as in_file:
+            for line in in_file.readlines():
+                self.data["X"].append(float(line.split("\t")[0]))
+                self.data["Y"].append(float(line.split("\t")[1]))
+        return self.data
+
--- visualize/visualize.py
+++ visualize/visualize.py
@@ -1,26 +1,17 @@
 import os, sys
 import matplotlib.pyplot as plt
+from preprocess import Preprocesser
 
 class Visualizer():
     
     def __init__(self, input=None):
         if input:
             self.get_data_from_file(input)
-    
+
     def get_data_from_file(self, input):
-        if not os.path.isfile(input):
-            print(input + " is not exists.")
-            sys.exit(-1)
-        self.input = input
-        with open(input) as in_file:
-            self.data = {
-                "X": [],
-                "Y": []
-            }
-            for line in in_file.readlines():
-                self.data["X"].append(float(line.split("\t")[0]))
-                self.data["Y"].append(float(line.split("\t")[1]))
-    
+        preprocesser = Preprocesser()
+        self.data = preprocesser.process(input)
+
     def show(self, size=1):
         plt.axis([min(self.data["X"]), max(self.data["X"]), min(self.data["Y"]), max(self.data["Y"])])
         plt.scatter(self.data["X"], self.data["Y"], s=size)
